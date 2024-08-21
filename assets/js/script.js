63cf5331cf7ecb2a5524f187f788d5fcef726f31
giovanna <torresssgio@gmail.com>
2024-08-21 12:31:27 -0300

    arrumando os numeros decimais

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,23 +20,17 @@ form.addEventListener('submit', function(event){
 
     if (bmi < 16.9){
         description = 'Muito abaixo do peso'
-    }
-    else if (bmi >= 17 && bmi <= 18,4){
+    } else if (bmi >= 17 && bmi <= 18.4){
         description = 'Abaixo do peso'
-    }
-    else if (bmi >= 18.5 && bmi <= 24.9){
+    } else if (bmi >= 18.5 && bmi <= 24.9){
         description = 'Peso normal'
-    }
-    else if (bmi >= 25 && bmi <= 29,9){
+    } else if (bmi >= 25 && bmi <= 29.9){
         description = 'Acima do peso'
-    }
-    else if (bmi >= 30 && bmi <= 34,9){
+    } else if (bmi >= 30 && bmi <= 34.9){
         description = "Obesidade grau I"
-    }
-    else if (bmi >= 25 && bmi <= 40){
+    } else if (bmi >= 25 && bmi <= 40){
         description = 'Obesidade grau II'
-    }
-    else{
+    } else{
         description = 'Obesidade grau III'
     }
 
